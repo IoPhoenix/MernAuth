fca8e2ab494f9a6ce7878f23cdb34f99172be061
Olga Fomin <olga.fomin26@gmail.com>
2019-08-17 14:29:53 -0700

    Use passport and jsonwebtokens for authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,13 +11,10 @@ const validateLoginInput = require("../../validation/login");
 // Load User model
 const User = require("../../models/User");
 
-
 // @route POST api/users/register
 // @desc Register user
 // @access Public
-
 router.post("/register", (req, res) => {
-
     // Form validation
     const { errors, isValid } = validateRegisterInput(req.body);
 
@@ -38,17 +35,68 @@ router.post("/register", (req, res) => {
                 password: req.body.password
             });
 
-        // Hash password before saving in database
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(newUser.password, salt, (err, hash) => {
-                if (err) throw err;
-                newUser.password = hash;
-                newUser
-                    .save()
-                    .then(user => res.json(user))
-                    .catch(err => console.log(err));
+            // Hash password before saving in database
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser
+                        .save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
                 });
             });
         }
     });
 });
+
+// @route POST api/users/login
+// @desc Login user and return JWT token
+// @access Public
+router.post("/login", (req, res) => {
+
+    // Form validation
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    // Check validation
+    if (!isValid) return res.status(400).json(errors);
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Find user by email
+    User.findOne({ email }).then(user => {
+        // Check if user exists
+        if (!user) return res.status(404).json({ emailnotfound: "Email not found" });
+
+        // Check password
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                // Create JWT Payload
+                const payload = {
+                    id: user.id,
+                    name: user.name
+                };
+                // Sign token
+                jwt.sign(
+                    payload,
+                    keys.secretOrKey,
+                    {
+                        expiresIn: 31556926 // year in seconds
+                    },
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: "Bearer " + token
+                        });
+                    }
+                );
+            } else {
+                return res.status(400).json({ passwordincorrect: "Password incorrect" });
+            }
+        });
+    });
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express"); // handle routing and requests
 const mongoose = require("mongoose"); // interact with MongoDB
 const bodyParser = require("body-parser"); // parse incoming request bodies
+const passport = require("passport"); // authenticate requests
+const users = require("./routes/api/users");
 const app = express();
 
 // Bodyparser middleware
@@ -23,6 +25,16 @@ mongoose
   )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
+
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
